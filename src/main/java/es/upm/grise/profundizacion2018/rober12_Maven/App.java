c07e12269781cae45aa1ace12a15b5ad21f9322b
Rober <robertgar1993@hotmail.com>
2018-12-04 17:30:37 +0100

    Maven

---
+++ src/main/java/es/upm/grise/profundizacion2018/rober12_Maven/App.java
@@ -0,0 +1,13 @@
+package es.upm.grise.profundizacion2018.rober12_Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
